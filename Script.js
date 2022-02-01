a9783804a6872572b5b149a9384fbca83326834d
SaidRasinlic <saidrasinlic.jr@gmail.com>
2022-02-01 14:23:28 +0100

    Update

--- Script.js
+++ Script.js
@@ -26,3 +26,37 @@ for (let i = 0; i < Menulinks.length; i += 1) {
 }
 
 // Mobile menu end////////////////////
+const nameID = document.getElementById('name');
+  const img = document.getElementById('img');
+  const companyName = document.getElementById('companyName');
+  const job = document.getElementById('job');
+  const year = document.getElementById('data');
+  const descrition = document.getElementById('description');
+  const tags = document.getElementById('tags');
+  const button1 = document.querySelector('#btn-1');
+  const button2 = document.querySelector('#btn-2');
+  const button3 = document.querySelector('#btn-3');
+  const button4 = document.querySelector('#btn-4');
+  const btnClosePop = document.querySelector('.popup-close-btn');
+  const popupOverlay = document.querySelector('.popup-overlay');
+  const popup = document.getElementById('popup');
+  function openPopUp() {
+    popup.classList.remove('hidden');
+  }
+  function changePopup(order) {
+    tags.innerHTML = '';
+    const project = data[`project${order}`];
+    nameID.innerHTML = project.name;
+    img.src = project.img;
+    companyName.innerHTML = project.companyName;
+    job.innerHTML = project.job;
+    year.innerHTML = project.year;
+    descrition.innerHTML = project.decription;
+    const tagsLength = project.tags.length;
+    for (let i = 0; i < tagsLength; i += 1) {
+      const element = document.createElement('li');
+      element.innerHTML = `${project.tags[i]}`;
+      tags.appendChild(element);
+    }
+    openPopUp();
+  }
